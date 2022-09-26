ee8b366f9e73ee0fbf6876f1734de1996d6da699
Petar Tzonevski <Pete@Petars-MBP-2.Home>
2022-09-26 04:41:55 -0400

    added button styling test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,16 @@
 /* eslint-disable */
 import React from 'react'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import EnhancedTable from '../table-components/EnhancedTable'
-import makeData from '../makeData'
-
-import { PillButton } from '@lmig/lmds-react-pill-button';
 
 const App = () => {
 
   return (
     <div>
       Hello
+      <button className="custom-btn btn-4">
+        <span>
+          Hello
+        </span>
+      </button>
     </div>
   )
 }
