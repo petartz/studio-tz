26942329d168bb0b8472359ecbaf078d1953fe5c
Petar Tzonevski <Pete@Petars-MBP-2.Home>
2022-09-26 10:13:42 -0400

    set-up-done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,11 +6,6 @@ const App = () => {
   return (
     <div>
       Hello
-      <button className="custom-btn btn-4">
-        <span>
-          Hello
-        </span>
-      </button>
     </div>
   )
 }
