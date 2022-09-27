cc86f1035414eaefb30f8da14605b52a79943cba
Petar Tzonevski <Pete@Petars-MBP-2.Home>
2022-09-27 05:24:23 -0400

    homepage layout of welcome message

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable */
 import React from 'react'
+import Welcome from '../components/home-components/Welcome'
 
 const App = () => {
 
   return (
     <div>
-      Hello
+      <Welcome/>
     </div>
   )
 }
