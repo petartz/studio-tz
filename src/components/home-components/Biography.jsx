b65f0f0e0f597f2b815ce8bf02c09958a3ddcca1
Him <him@Petars-MBP.lan>
2024-04-09 00:12:27 -0400

    panels

--- src/components/home-components/Biography.jsx
+++ src/components/home-components/Biography.jsx
@@ -2,15 +2,16 @@ const Biography = () => {
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2">
-        <p className="mx-4 mb-6 text-xs md:m-auto md:p-16">
-          a gumbo of interests boil and meld their flavors. linguistics, philosophy of mind,
-          a dash of jorge luis borges. dystopian novels that i haven't read,
-          weird strings of a conscious web. buena vista and calle13 a day keep
-          the doctor at bay. тютюн reminds me of 1984. childrens books to write and read,
-          ян бибиян, le petite prince?, фют o fausto, pero candide, güey desgraciado,
-          non-stop como que tiene gusano en los pompis. tarkovsky directed it.
+        <p className="mx-4 mb-6 text-xs md:m-auto md:p-16 lg:m-6">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+         when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+         It has survived not only five centuries, but also the leap into electronic typesetting, 
+         remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+         containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+         including versions of Lorem Ipsum.
         </p>
-        <img className="biopic m-auto mb-2" src="https://i.imgur.com/A3Vtb2k.jpg" alt="petar-biopic" title="source: imgur.com" />
+        <img className="biopic m-auto" src="https://i.imgur.com/A3Vtb2k.jpg" alt="petar-biopic" title="source: imgur.com" />
       </div>
     </div>
   )
---
+++ src/panels/SideBar.jsx
@@ -0,0 +1,10 @@
+const SideBar = (props) => {
+  return (
+    <div>
+        
+    </div>
+
+  )
+}
+
+export default SideBar
